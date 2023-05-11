abb526d29af25f61f505031e710945c42681d59c
Pankaj1656 <pc69269@gmail.com>
2023-05-11 23:18:02 +0530

    final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
-public class Main {
+import java.sql.SQLOutput;
 
+public class Main {
+         public static void main(String []args){
+             Product p=new Product();
+             System.out.println(p.product(5,8));
+             System.out.println(p.product(1,5,6));
+             System.out.println(p.product(9.5,10.2));
+         }
 }
