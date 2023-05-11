c6608e5ead76414bc7b4d140370fdef36a29ca04
Pankaj1656 <pc69269@gmail.com>
2023-05-11 23:23:41 +0530

    final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,8 +5,8 @@ import java.sql.SQLOutput;
 public class Main {
          public static void main(String []args){
              Product p=new Product();
-             System.out.println(p.product(5,8));
-             System.out.println(p.product(1,5,6));
-             System.out.println(p.product(9.5,10.2));
+             p.product(5,8);
+             p.product(1,5,6);
+             p.product(9,10);
          }
 }
