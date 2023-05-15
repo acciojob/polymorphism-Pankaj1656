50b97eec08d5b587761e6a64c3404d551f5fb4e8
Pankaj1656 <pc69269@gmail.com>
2023-05-15 11:27:20 +0530

    Final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,19 @@ package com.driver;
 import java.sql.SQLOutput;
 
 public class Main {
-         public static void main(String []args){
+    public static  class Product {
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
+
+    public static void main(String []args){
              Product p=new Product();
             System.out.println( p.product(5,8));
              System.out.println(p.product(1,5,6));
